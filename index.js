042481989c59ae26419a964ed25caccde142d516
VerdyNordsten <verdynordsten97@gmail.com>
2023-02-17 17:49:05 +0800

    updated cors

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(morgan("dev"))
 app.use(helmet())
 app.use(xss())
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  next()
+})
+
 app.use("/api/v1", mainRouter)
 
 // app.use("/img", express.static("upload"))
