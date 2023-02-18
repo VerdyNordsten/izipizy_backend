442e08d99b8cb337a7ebac3642bf49125aabf674
RezaldhoArmadhani <rezaldhoarmadhani11@gmail.com>
2023-02-18 12:09:46 +0700

    improve update profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,35 +1,36 @@
 /* eslint-disable no-unused-vars */
 const userModel = require("../models/userModel");
-const uuid = require('uuid');
+const uuid = require("uuid");
 const commonHelper = require("../helper/common");
 const authHelper = require("../helper/auth");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+var cloudinary = require("../config/cloudinary");
 
 const userController = {
   registerUser: async (req, res) => {
-    try{
-        const { name, email, password, phone } = req.body;
-        const checkEmail = await userModel.findEmail(email);
-        if (checkEmail.rowCount > 0) {
-            return res.json({
-                message: 'Email already exist'
-            });
-        }
-        const hashPassword = await bcrypt.hash(password, saltRounds);
-        const id = uuid.v4();
-        const data = {
-            id,
-            name,
-            email,
-            password: hashPassword,
-            phone_number : phone
-        }
-        const result = await userModel.insertUser(data);
-        commonHelper.response(res, result.rows, 201, 'Register has been success')
-    }catch(err){
-        res.send(err)
+    try {
+      const { name, email, password, phone } = req.body;
+      const checkEmail = await userModel.findEmail(email);
+      if (checkEmail.rowCount > 0) {
+        return res.json({
+          message: "Email already exist",
+        });
+      }
+      const hashPassword = await bcrypt.hash(password, saltRounds);
+      const id = uuid.v4();
+      const data = {
+        id,
+        name,
+        email,
+        password: hashPassword,
+        phone_number: phone,
+      };
+      const result = await userModel.insertUser(data);
+      commonHelper.response(res, result.rows, 201, "Register has been success");
+    } catch (err) {
+      res.send(err);
     }
   },
 
@@ -55,14 +56,14 @@ const userController = {
         email: user.email,
         id: user.id, // add the user ID to the payload
       };
-      console.log(payload)
+      console.log(payload);
       user.token = authHelper.generateToken(payload);
       user.refreshToken = authHelper.generateRefreshToken(payload);
       commonHelper.response(res, user, 201, "login is successful");
     } catch (err) {
       res.send(err);
     }
-  },  
+  },
 
   refreshToken: (req, res) => {
     const refreshToken = req.body.refreshToken;
@@ -84,6 +85,30 @@ const userController = {
     } = await userModel.findEmail(email);
     delete user.password;
     commonHelper.response(res, user, 200, "Get data profile is successful");
+  },
+
+  editProfile: async (req, res) => {
+    const email = req.payload.email;
+    const { password } = req.body;
+    const hashPassword = await bcrypt.hash(password, saltRounds);
+    // const imageUrl = await cloudinary.uploader.upload(req.file.path, {
+    //   folder: "izipizy",
+    // });
+    let photo;
+    if (req.file) {
+      const imageUrl = await cloudinary.uploader.upload(req.file.path, {
+        folder: "izipizy",
+      });
+      photo = imageUrl.secure_url;
+    }
+    const result = await userModel.editProfile(hashPassword, photo, email);
+    const dataProfile = await userModel.findEmail(email);
+    commonHelper.response(
+      res,
+      dataProfile.rows[0],
+      200,
+      "Get data profile is successful"
+    );
   },
 };
 
--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,39 @@
-const Pool = require('../config/db');
+const Pool = require("../config/db");
 
 const findId = (id) => {
-  return Pool.query('SELECT * FROM users WHERE id = $1', [id]);
-}
+  return Pool.query("SELECT * FROM users WHERE id = $1", [id]);
+};
 
 const findEmail = (email) => {
-  return Pool.query('SELECT * FROM users WHERE email = $1', [email]);
-}
+  return Pool.query("SELECT * FROM users WHERE email = $1", [email]);
+};
 
 const insertUser = async (data) => {
-  return await Pool.query('INSERT INTO users (id, name, email, password, phone_number) VALUES ($1, $2, $3, $4, $5)', [data.id, data.name, data.email, data.password, data.phone_number]);
-}
+  return await Pool.query(
+    "INSERT INTO users (id, name, email, password, phone_number) VALUES ($1, $2, $3, $4, $5)",
+    [data.id, data.name, data.email, data.password, data.phone_number]
+  );
+};
 
-module.exports = { findId, findEmail, insertUser}
+// const editProfile = async (password, photo, email) => {
+//   return await Pool.query(
+//     `UPDATE users SET image_profile='${photo}', password='${password}' WHERE email='${email}'`
+//   );
+// };
+
+const editProfile = (password, photo, email) => {
+  return new Promise((resolve, reject) =>
+    Pool.query(
+      `UPDATE users SET image_profile='${photo}', password='${password}' WHERE email='${email}'`,
+      (error, result) => {
+        if (!error) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    )
+  );
+};
+
+module.exports = { findId, findEmail, insertUser, editProfile };
